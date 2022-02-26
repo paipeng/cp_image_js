0d652b40c12886a68575987fd196515e81515778
Pai Peng <sipaipv6@gmail.com>
2022-02-26 16:43:26 +0800

    add bmp read/write

---
+++ bmp/cp_bmp.js
@@ -0,0 +1,15 @@
+/**
+ * @author Pai Peng
+ *
+ * gray color
+ * 
+ * 
+ */
+
+//var writer = require('./cp_bmp_write');
+var reader = require('./cp_bmp_read');
+
+module.exports = {
+    //writer: writer,
+    reader: reader
+};
