b38b78f97e4e9704ff2330a8da4dba8daa61a799
Pai Peng <sipaipv6@gmail.com>
2022-03-05 17:29:41 +0800

    update

--- detect_contour.js
+++ detect_contour.js
@@ -15,17 +15,25 @@ log.text.print('mean: ' + mean + '\n');
 var binaryMat = imageProcess.util().binary(mat, mean * 0.8);
 bmp.writer('./output/detect_binary.bmp', binaryMat);
 
-var invertMat = imageProcess.util().invert(binaryMat);
-//log.mat.print(invertMat);
-bmp.writer('./output/invert_mat.bmp', invertMat);
-
 
 
 // 5.1 labeling
 var labelMat = imageProcess.label(binaryMat).label({ x: binaryMat.width / 2, y: binaryMat.height / 2 });
 bmp.writer('./output/label_mat.bmp', labelMat);
 
+var invertMat = imageProcess.util().invert(labelMat);
+//log.mat.print(invertMat);
+bmp.writer('./output/invert_mat.bmp', invertMat);
+
+
+
 // 6. find contour
-var points = imageProcess.contour(labelMat).findContour();
-var drawMat = imageProcess.draw(binaryMat).drawPoints(points, 0);
-bmp.writer('./output/detect_contour_points_draw.bmp', drawMat);
+var points = imageProcess.contour(invertMat).findContour();
+
+var drawMat = imageProcess.draw(mat).drawPoints(points, 0);
+bmp.writer('./output/detect_contour.bmp', drawMat);
+
+// get avg rectangle shape
+var avgRectangle = imageProcess.contour(binaryMat).getAvgShape(points);
+var drawShapeMat = imageProcess.draw(drawMat).drawRectangleOnMat(mat, avgRectangle, 60);
+bmp.writer('./output/detect_contour_shape.bmp', drawShapeMat);
