d445808843ce6453fc7685099a2b4ff6dbd77127
Pai Peng <sipaipv6@gmail.com>
2022-03-06 13:04:23 +0800

    calculate sharpness within given rect area (SMD)

--- imageprocess/lib/cp_sharpness.js
+++ imageprocess/lib/cp_sharpness.js
@@ -15,6 +15,19 @@ CPSharpness.prototype.SMD = function () {
 
 };
 
+
+CPSharpness.prototype.sharpness = function (rect) {
+    var out = 0;
+    for (var i = rect.top; i < rect.bottom; i++) {
+        for (var j = rect.left; j < rect.right; j++) {
+            out += Math.abs(this.mat.data[i * this.mat.width + j] - this.mat.data[(i + 1) * this.mat.width + j]);
+            out += Math.abs(this.mat.data[i * this.mat.width + j] - this.mat.data[i * this.mat.width + j + 1]);
+        }
+    }
+    return out / ((rect.right - rect.left) * (rect.bottom - rect.top));
+
+};
+
 module.exports = function (mat) {
     var sharpness = new CPSharpness(mat);
     return sharpness;
