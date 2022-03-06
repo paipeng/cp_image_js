6c24d4fc7bdbf6c06ee3ae9617f49851226343e1
Pai Peng <sipaipv6@gmail.com>
2022-03-06 09:22:27 +0800

    add image process function (crop + resize)

--- imageprocess/cp_imageprocess.js
+++ imageprocess/cp_imageprocess.js
@@ -12,6 +12,37 @@ var dilate = require('./lib/cp_dilate');
 var erosion = require('./lib/cp_erosion');
 var label = require('./lib/cp_label');
 
+
+function CPIP() {
+
+}
+CPIP.prototype.create = function (mat, cropX, cropY, cropWidth, cropHeight, resizeWidth, resizeHeight) {
+    var cropPixels = new Uint8Array(cropWidth * cropHeight);
+    // set to white !
+
+    // crop pixels
+    for (var i = cropY; i < cropHeight; i++) {
+        for (var j = cropX; j < cropWidth; j++) {
+            if (i >= 0 && i < mat.height && j >= 0 && j < mat.width) {
+                cropPixels[(i - cropY) * mat.height * mat.channel + (j - cropX)] = mat.data[i * mat.height * mat.channel + j]
+            }
+        }
+    }
+
+    // resize
+    if (resizeWidth == cropWidth && resizeHeight == cropHeight) {
+        return resize(mat).resize(resizeWidth, resizeHeight);
+    } else {
+        return {
+            width: mat.width,
+            height: mat.height,
+            channel: 1,
+            data: temp
+        };
+    }
+};
+
+
 module.exports = {
     resize: resize,
     crop: crop,
@@ -23,5 +54,6 @@ module.exports = {
     sharpness: sharpness,
     dilate: dilate,
     erosion: erosion,
-    label: label
+    label: label,
+    process: new CPIP()
 };
