9ced3c1dabcc137ba5b75ee931303eebed35ea75
Pai Peng <sipaipv6@gmail.com>
2022-03-06 20:22:14 +0800

    fix error

--- imageprocess/lib/cp_util.js
+++ imageprocess/lib/cp_util.js
@@ -8,8 +8,8 @@ function CpUtil() {
 CpUtil.prototype.binary = function (mat, threshold) {
     var temp = new Uint8Array(mat.width * mat.height);
     for (var i = 0; i < mat.height; i++) {
-        for (var j = 0; j < mat.height * mat.channel; j++) {
-            temp[i * mat.height * mat.channel + j] = (mat.data[i * mat.height * mat.channel + j] > threshold) ? 255 : 0;
+        for (var j = 0; j < mat.width * mat.channel; j++) {
+            temp[i * mat.width * mat.channel + j] = (mat.data[i * mat.width * mat.channel + j] > threshold) ? 255 : 0;
         }
     }
     return {
@@ -23,8 +23,8 @@ CpUtil.prototype.binary = function (mat, threshold) {
 CpUtil.prototype.invert = function (mat) {
     var temp = new Uint8Array(mat.width * mat.height);
     for (var i = 0; i < mat.height; i++) {
-        for (var j = 0; j < mat.height * mat.channel; j++) {
-            temp[i * mat.height * mat.channel + j] = 255 - mat.data[i * mat.height * mat.channel + j];
+        for (var j = 0; j < mat.width * mat.channel; j++) {
+            temp[i * mat.width * mat.channel + j] = 255 - mat.data[i * mat.width * mat.channel + j];
         }
     }
     return {
