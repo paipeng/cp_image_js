c7aa8892904662e676722718bb1856a0e3b6db7b
Pai Peng <sipaipv6@gmail.com>
2022-03-31 23:05:28 +0800

    add to cp_mat util class

--- test/tiff.test.js
+++ test/tiff.test.js
@@ -1,7 +1,7 @@
 
 var fs = require('fs');
 var { decode } = require('tiff');
-
+var cp_image = require("../index");
 
 var imageFile = './images/unesco-logo.tif';
 
@@ -20,12 +20,26 @@ afterEach(() => {
 test('read tiff image', () => {
     var tiff_data = fs.readFileSync(imageFile);
     console.log(tiff_data);
-    const [result] = decode(tiff_data);
-    console.log(result.width);
-    console.log(result.height);
-    console.log(result.components);
+    const [mat] = decode(tiff_data);
+    console.log(mat.width);
+    console.log(mat.height);
+    console.log(mat.components);
+    expect(mat.width).toBe(138);
+    expect(mat.height).toBe(98);
+    expect(mat.components).toBe(3);
 });
 
 
+
+
+test('read tiff image via cp_image', () => {
+    var mat = cp_image.matUtil.readMat(imageFile);
+    console.log(mat.width);
+    console.log(mat.height);
+    console.log(mat.channel);
+    expect(mat.width).toBe(138);
+    expect(mat.height).toBe(98);
+    expect(mat.channel).toBe(3);
+});
 
 
