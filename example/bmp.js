1152925cdba5dba8ea32502a635eb53f17cde2d0
Pai Peng <sipaipv6@gmail.com>
2022-04-01 15:14:38 +0800

    update

--- example/bmp.js
+++ example/bmp.js
@@ -1,4 +1,9 @@
 var cp_image = require("../index");
 
 var mat = cp_image.matUtil.createMat(20, 20, 3);
+
+
+mat.data[42] = 0;
+mat.data[43] = 0;
+mat.data[44] = 0;
 cp_image.matUtil.writeBmpMat('./output/mat_color.bmp', mat);
