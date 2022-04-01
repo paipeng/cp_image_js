6449295f69f9c00e936826673237b881c758d54f
Pai Peng <sipaipv6@gmail.com>
2022-04-01 10:05:52 +0800

    update unit test

--- test/draw.test.js
+++ test/draw.test.js
@@ -4,13 +4,18 @@ var { CPLine, CPPoint, matUtil, CPUtil, CPDraw } = require("../index");
 var points = [
 ];
 
+beforeEach(() => {
+    points.push(new CPPoint(10, 10));
+    points.push(new CPPoint(40, 10));
+    points.push(new CPPoint(40, 40));
+});
 
+afterEach(() => {
+
+});
 
 
 test('draw line', () => {
-    points.push(new CPPoint(207, 184));
-    points.push(new CPPoint(393, 60));
-
     var cpUtil = new CPUtil();
     var maxX = cpUtil.getMaxX(points);
     var maxY = cpUtil.getMaxY(points);
@@ -22,6 +27,22 @@ test('draw line', () => {
     cpDraw.drawLine(mat, new CPLine(points[0], points[1]), 0);
 
     matUtil.writeBmpMat('./output/draw_line.bmp', mat);
+
+});
+
+
+test('draw polygon', () => {
+    var cpUtil = new CPUtil();
+    var maxX = cpUtil.getMaxX(points);
+    var maxY = cpUtil.getMaxY(points);
+    console.log('maxX', maxX, maxY);
+    var mat = matUtil.createMat(maxX + 20, maxY + 20, 1);
+
+    var cpDraw = new CPDraw();
+    //cpDraw.drawPoints(mat, points, 0);
+    cpDraw.drawPolygon(mat, points, 0);
+
+    matUtil.writeBmpMat('./output/draw_polygon.bmp', mat);
 
 
 
