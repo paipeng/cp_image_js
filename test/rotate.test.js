86f1afa5bca3745da40522d7c2a75b1d47dd9764
Pai Peng <sipaipv6@gmail.com>
2022-04-01 23:12:31 +0800

    add new test

--- test/rotate.test.js
+++ test/rotate.test.js
@@ -14,7 +14,7 @@ afterEach(() => {
 
 });
 
-test('rotate 90', () => {
+test.only('rotate 90', () => {
     var rotate = new CPRotate();
     var mat = rotate.rotate(grayMat, 90);
 
@@ -30,10 +30,35 @@ test('rotate 180', () => {
 });
 
 
+test('rotate 15', () => {
+    var rotate = new CPRotate();
+    var mat = rotate.rotate(grayMat, 15);
+
+    matUtil.writeBmpMat('./output/rotate_15.bmp', mat);
+});
+
+
 test('rotate 45', () => {
     var rotate = new CPRotate();
     var mat = rotate.rotate(grayMat, 45);
 
     matUtil.writeBmpMat('./output/rotate_45.bmp', mat);
+});
+
+
+test('rotate 60', () => {
+    var rotate = new CPRotate();
+    var mat = rotate.rotate(grayMat, 60);
+
+    matUtil.writeBmpMat('./output/rotate_60.bmp', mat);
+});
+
+
+
+test('rotate 80', () => {
+    var rotate = new CPRotate();
+    var mat = rotate.rotate(grayMat, 80);
+
+    matUtil.writeBmpMat('./output/rotate_80.bmp', mat);
 });
 
