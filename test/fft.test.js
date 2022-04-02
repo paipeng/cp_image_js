5cd8be630bf682a23e250c4f5271f06fedacef71
Pai Peng <sipaipv6@gmail.com>
2022-04-02 13:33:39 +0800

    fft2

--- test/fft.test.js
+++ test/fft.test.js
@@ -2,7 +2,9 @@ var { matUtil, imageprocess, CPUtil, CPDraw, CPFFT } = require("../index");
 
 
 var grayMat = null;
+var fft = null;
 beforeEach(() => {
+    fft = new CPFFT();
     var imageFile = './images/find_contour.bmp';
     console.log('test image path: ' + imageFile);
 
@@ -16,9 +18,7 @@ afterEach(() => {
 
 
 
-test.only('fft 1d', () => {
-    var fft = new CPFFT();
-
+test('fft 1d', () => {
     var points = [];
     points.push(10);
     points.push(1);
@@ -38,5 +38,42 @@ test.only('fft 1d', () => {
 
     complexs2 = fft.ifft1(complexs);
     console.log(complexs2);
+
+});
+
+
+
+
+test.only('fft2', () => {
+    var points = [];
+    points.push([1, 2, 3, 4, 5, 6, 7, 8]);
+    points.push([8, 7, 6, 5, 4, 3, 2, 1]);
+    points.push([5, 6, 7, 8, 1, 2, 3, 4]);
+    points.push([1, 2, 3, 4, 8, 7, 6, 5]);
+    points.push([2, 2, 3, 4, 8, 7, 6, 5]);
+    points.push([3, 2, 3, 4, 8, 7, 6, 5]);
+    points.push([4, 2, 3, 4, 8, 7, 6, 5]);
+    points.push([5, 2, 3, 4, 8, 7, 6, 5]);
+
+
+
+    points = [];
+    points.push([1, 2, 3, 4]);
+    points.push([4, 3, 2, 1]);
+    points.push([5, 6, 7, 8]);
+    points.push([9, 7, 5, 3]);
+
+    console.log(points[2][1]);
+    console.log(points[0].length);
+    points = fft.convertToCPComplexArray2D(points);
+    console.log(points[0].length);
+
+
+    for (var i = 0; i < points.length; i++) {
+        console.log(points[i]);
+    }
+
+    complexs = fft.fft2(points);
+    console.log(complexs)
 
 });
