acdcd3e587fd4856774e8181ca1fc9d25121837f
Pai Peng <sipaipv6@gmail.com>
2022-04-02 10:09:52 +0800

    add a new test

--- test/harris.test.js
+++ test/harris.test.js
@@ -16,9 +16,18 @@ afterEach(() => {
 
 
 
-test('rotate 90', () => {
+test('harris 3', () => {
     var harris = new CPHarrisCorner();
     var mat = harris.findCorners(grayMat, 3);
 
     matUtil.writeBmpMat('./output/harris_corner.bmp', mat);
+});
+
+
+
+test('harris 1', () => {
+    var harris = new CPHarrisCorner();
+    var mat = harris.findCorners(grayMat, 1);
+
+    matUtil.writeBmpMat('./output/harris_corner_1.bmp', mat);
 });
